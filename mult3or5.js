8df299d46f67fc3a7270d182d3b7a2093261410d
benochi <73758508+benochi@users.noreply.github.com>
2021-06-17 07:34:58 -0500

    Update mult3or5.js

--- mult3or5.js
+++ mult3or5.js
@@ -2,7 +2,7 @@ function solution(number){
   let sum = 0;
   for (let i = 0; i < number; i++)
     if (i % 3 === 0 || i % 5 === 0){
-    sum = i + sum
+    sum += i; //same as sum = i + sum
   }
   return sum;
 }
